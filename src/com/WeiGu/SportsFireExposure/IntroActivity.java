2b740ed70701758a1b24b32ce17145e3c3b26858
elliott-omosheye <user1@dhcp-42-33.xlate.eduroam.ucl.ac.uk>
2013-07-08 14:05:36 +0100

    Wei initial work

---
+++ src/com/WeiGu/SportsFireExposure/IntroActivity.java
@@ -0,0 +1,46 @@
+package com.WeiGu.SportsFireExposure;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.WeiGu.SporysFireExposure.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class IntroActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
+		setContentView(R.layout.intro_activity);
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask()
+		{
+			public void run()
+			{
+				Intent myIntent = new Intent(IntroActivity.this, MainActivity.class);
+				IntroActivity.this.startActivity(myIntent);
+			}
+		}, 2000);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
